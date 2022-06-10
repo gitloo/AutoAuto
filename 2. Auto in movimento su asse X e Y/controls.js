d1a852002d0d3a084cf1ff0b57893ad52a5bdaf7
gitloo <97623327+gitloo@users.noreply.github.com>
2022-06-10 18:53:09 +0200

    updated - controls.js rimossi console debug frecce

--- 2. Auto in movimento su asse X e Y/controls.js
+++ 2. Auto in movimento su asse X e Y/controls.js
@@ -27,7 +27,7 @@ class Controls{
                     this.reverse=true;
                     break;       
             }
-            console.table(this);
+            // console.table(this); debugger per il controllo funzionamento tasti frecce
         }
         document.onkeyup=(event)=>{
             switch(event.key){
@@ -44,7 +44,7 @@ class Controls{
                     this.reverse=false;
                     break;       
             }
-            console.table(this);
+            // console.table(this); debugger per il controllo funzionamento tasti frecce
         }
     } 
 }
