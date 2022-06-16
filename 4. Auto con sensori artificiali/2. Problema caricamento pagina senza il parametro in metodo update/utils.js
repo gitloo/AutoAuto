ea46391fee24e52e50b3b09e233b0d4a7a4a753d
gitloo <97623327+gitloo@users.noreply.github.com>
2022-06-16 20:11:25 +0200

    added - Problema caricamento index
    Problema di visualizzazione della pagina index in assenza del parametro roadBorders nel metodo update nel file sensor.js

---
+++ 4. Auto con sensori artificiali/2. Problema caricamento pagina senza il parametro in metodo update/utils.js
@@ -0,0 +1,28 @@
+// funzione che ci restituisce un valore compreso tra A e B, a seconda del parametro t inserito
+function lerp(A,B,t) {
+    return A+(B-A)*t;
+    // con t=0 --> il valore returnato è A
+    // con t=1 --> A e -A si annulleranno a vicenda e il valore returnato sarà B
+    // con 0<t<1 --> 
+}
+
+
+function getIntersection(A,B,C,D){
+    const tTop = (D.x - C.x)*(A.y - C.y) - (D.y - C.y)*(A.x - C.x);
+    const uTop = (C.y - A.y)*(A.x - B.x) - (C.x - A.x)*(A.y - B.y);
+    const bottom = (D.y - C.y)*(B.x - A.x) - (D.x - C.x)*(B.y - A.y);
+
+    if(bottom!=0){
+        const t = tTop / bottom;
+        const u = uTop / bottom;
+        if(t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+            return {
+                x : lerp(A.x, B.x, t),
+                y : lerp(A.y, B.y, t),
+                offset : t
+            }
+        } 
+    }
+
+    return null;
+}
