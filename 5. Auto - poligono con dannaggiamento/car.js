8ace6da10739557a60db06667e833ded44b06af7
gitloo <97623327+gitloo@users.noreply.github.com>
2022-06-17 13:36:31 +0200

    added - Auto poligono con danneggiamento
    L'auto ora viene disegnata come poligono con punti definiti, grazie ai quali è stato possibile aggiungere il danneggiamento.

---
+++ 5. Auto - poligono con dannaggiamento/car.js
@@ -0,0 +1,129 @@
+class Car{
+    constructor(x,y,width,height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.speed=0;
+        this.acceleration=0.2;
+        this.maxSpeed=3;
+        this.friction=0.05;
+        this.angle=0;
+        this.damaged=false
+
+        // come parametro per la classe Sensor dovremmo passare un oggetto Car e con this riusciamo a richiamare tutte le proprietà di questa classe 
+        this.sensor = new Sensor(this);
+        this.controls = new Controls();
+    }
+
+    update(roadBorders){
+        if(!this.damaged){ // se la macchina si danneggia non deve potersi muovere
+            this.#move(); // # davanti a un metodo lo rende privato, accessibile soltanto dalla classe che lo implementa
+            this.polygon = this.#createPolygon();
+            this.damaged=this.#assesDamage(roadBorders);
+        }
+        this.sensor.update(roadBorders);
+    }
+
+    #assesDamage(roadBorders){
+        for(let i = 0; i < roadBorders.length; i++) { // cicliamo lungo tutti i bordi
+            if(polysIntersect(this.polygon,roadBorders[i])){ // se il poligono interseca i bordi (segmento) della strada restituisci true a this.damaged
+                return true;
+            }
+        }
+        return false;
+    }
+
+    #createPolygon(){
+        const points=[];
+        const rad = Math.hypot(this.width,this.height)/2; // otteniamo la metà dell'ipotenusa derivata da larghezza e altezza del rettangolo della macchina
+        const alpha = Math.atan2(this.width, this.height); // otteniamo l'angolo compreso tra ipotenusa e altezza
+        // top-right point del rettangolo 
+        points.push({
+            x : this.x - Math.sin(this.angle - alpha)*rad, // x del centro della macchina - (seno di (0 - arcotangente dell'ipotenusa)) * ipotenusa/2
+            y : this.y - Math.cos(this.angle - alpha)*rad, // y del centro della macchina - (coseno di (0 - arcotangente dell'ipotenusa)) * ipotenusa/2
+        });         
+        // top-left point del rettangolo 
+        points.push({
+            x : this.x - Math.sin(this.angle + alpha)*rad, // x del centro della macchina - (seno di (0 + arcotangente dell'ipotenusa)) * ipotenusa/2
+            y : this.y - Math.cos(this.angle + alpha)*rad, // y del centro della macchina - (coseno di (0 + arcotangente dell'ipotenusa)) * ipotenusa/2
+        });         
+        // bottom-right point del rettangolo 
+        points.push({
+            x : this.x - Math.sin(Math.PI + this.angle - alpha)*rad, // x del centro della macchina - (seno di (180° + this.angle - arcotangente dell'ipotenusa)) * ipotenusa/2
+            y : this.y - Math.cos(Math.PI + this.angle - alpha)*rad, // y del centro della macchina - (coseno di (180° + this.angle - arcotangente dell'ipotenusa)) * ipotenusa/2
+        });         
+        //bottom-left point del rettangolo 
+        points.push({
+            x : this.x - Math.sin(Math.PI + this.angle + alpha)*rad, // x del centro della macchina - (seno di (180° + this.angle + arcotangente dell'ipotenusa)) * ipotenusa/2
+            y : this.y - Math.cos(Math.PI + this.angle + alpha)*rad, // y del centro della macchina - (coseno di (180° + this.angle + arcotangente dell'ipotenusa)) * ipotenusa/2
+        });
+
+        return points;
+    }
+
+    #move(){
+        if(this.controls.forward){
+            this.speed+=this.acceleration;
+        }
+        if(this.controls.reverse){
+            this.speed-=this.acceleration;
+        }
+        if(this.speed>this.maxSpeed){
+            this.speed=this.maxSpeed;
+        }
+        if(this.speed<-this.maxSpeed/2){
+            this.speed=-this.maxSpeed/2;
+        }
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+        if(Math.abs(this.speed)<this.friction){ // se il valore assoluto (sia in avanzamento che retromarcia) è minore della frizione (cioè meno di 0.05), allora la velocità diventa 0
+            this.speed=0;
+        }
+        if(this.speed!=0){
+            const flip = this.speed>0?1:-1 
+            if(this.controls.left){
+                this.angle+=0.03*flip; // quando la macchina ha una velocità positiva l'aggiunta di flip non cambierà nulla in quanto il suo valore è 1.
+            }
+            if(this.controls.right){
+                this.angle-=0.03*flip;  // quando la macchina procederà all'indietro l'aggiunta di flip (con valore -1) invertirà il segno della sottrazione (-0.03 diventerà 0.03).
+            }
+            // il valore di flip dipenderà dalla velocità: se speed è maggiore di 0 flip sarà 1 | se speed è 0 flip sarà -1. In questo modo sarà possibile invertire la rotazione nel momento in cui si procede in retromarcia
+        }
+
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
+    }
+
+    draw(ctx) { 
+/*      ctx.save(); // salviamo il canvas context
+        ctx.translate(this.x, this.y); // trasliamo il rettangolo alla coordinata(x,y) in cui vogliamo centrare la rotazione
+        ctx.rotate(-this.angle); // ruotiamo il rettangolo
+        ctx.beginPath(); 
+        // rimuoviamo this.x e this.y perchè abbiamo già traslato il rettangolo alla coordinata che ci interessa
+        ctx.rect(-this.width/2, -this.height/2, this.width, this.height);
+
+        ctx.fill(); 
+        ctx.restore(); // chiamiamo il metodo restore() perchè altrimenti si entrerebbe in un loop 
+        // di traslazione e rotazione ad ogni frame dell'animazione 
+*/
+//      INVECE DI DISEGNARE IL RETTANGOLO ORA DISEGNIAMO IL POLIGONO CON I PUNTI CHE ABBIAMO CREATO 
+        if(this.damaged){
+            ctx.fillStyle="gray";
+        } else {
+            ctx.fillStyle="black";
+        }
+        ctx.beginPath();
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        for(let i = 0; i < this.polygon.length; i++){
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+        }
+        ctx.fill();
+        this.sensor.draw(ctx);
+    }
+}
---
+++ 5. Auto - poligono con dannaggiamento/utils.js
@@ -0,0 +1,52 @@
+// funzione che ci restituisce un valore compreso tra A e B, a seconda del parametro t inserito
+function lerp(A,B,t) {
+    return A+(B-A)*t;
+    // con t=0 --> il valore returnato è A
+    // con t=1 --> A e -A si annulleranno a vicenda e il valore returnato sarà B
+    // con 0<t<1 --> 
+}
+
+
+function getIntersection(A,B,C,D){
+    const tTop = (D.x - C.x)*(A.y - C.y) - (D.y - C.y)*(A.x - C.x);
+    const uTop = (C.y - A.y)*(A.x - B.x) - (C.x - A.x)*(A.y - B.y);
+    const bottom = (D.y - C.y)*(B.x - A.x) - (D.x - C.x)*(B.y - A.y);
+
+    if(bottom!=0){
+        const t = tTop / bottom;
+        const u = uTop / bottom;
+        if(t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+            return {
+                x : lerp(A.x, B.x, t),
+                y : lerp(A.y, B.y, t),
+                offset : t
+            }
+        } 
+    }
+
+    return null;
+}
+
+function polysIntersect(poly1, poly2) {
+    for(let i = 0; i < poly1.length; i++){
+        for(let j = 0; j < poly2.length; j++){
+            const touch = getIntersection( // con questa funzione prendiamo tutti i segmenti che compongono il primo poligono e li compariamo con tutti i segmenti del secondo poligono 
+                                           // (nel caso dei bordi della strada, il "poligono" è composto da un solo segmento)
+                // prendiamo il primo punto del primo poligono
+                poly1[i], 
+                // prendiamo il punto successivo del primo poligono, ma i+1 darebbe errore in quanto andrebbe a superare la condizione i<poly1.length,
+                // andando oltre i limiti dell'array poly1. Aggiungendo l'operatore modulo, indichiamo che quando il valore di i raggiungerà il numero
+                // di punti da cui è costituito il poligono, cioè poly1.length, quel valore diventerà 0, cioè l'ultimo punto si unirà al primo punto del poligono, completandolo.
+                poly1[(i+1) % poly1.length],
+                // prendiamo il primo punto del secondo poligono
+                poly2[j],
+                // prendiamo l'ultimo punto del del secondo poligono
+                poly2[(j+1) % poly2.length],
+            )
+            if(touch){
+                return true;
+            }
+        }
+    }
+    return false;
+}
